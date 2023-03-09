8a82e8c825e28bc90773b0fdb0b51fdc2c092015
Ivan-Urenda <81488477+Ivan-Urenda@users.noreply.github.com>
2023-03-08 22:02:16 -0600

    Initial commit

---
+++ app/src/main/java/com/urenda/animaladoption/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.urenda.animaladoption
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
